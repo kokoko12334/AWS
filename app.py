931aa1337f4dfedeb7b6868d9e25baf188d8500f
kokoko12334 <gnjgnjgnj1@gmail.com>
2022-04-18 14:55:35 +0900

    e

---
+++ app.py
@@ -0,0 +1,132 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+import pickle
+import numpy as np
+import json
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import urllib.request
+import time
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from Transformer import *
+
+#tokenizer 값 불러오기
+
+import pickle
+model_path = 'C:/Users/user/Desktop/web/python-deep-learning-chatbot/'
+with open (model_path + 'tokenizer.pkl', 'rb') as f:
+  tokenizer = pickle.load(f)
+# 시작 토큰과 종료 토큰에 대한 정수 부여.
+START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
+
+# 시작 토큰과 종료 토큰을 고려하여 단어 집합의 크기를 + 2
+VOCAB_SIZE = tokenizer.vocab_size + 2
+####Transformer 모델 생성
+
+# 하이퍼파라미터
+D_MODEL = 256
+NUM_LAYERS = 2
+NUM_HEADS = 8
+DFF = 512
+DROPOUT = 0.1
+model = transformer(
+    vocab_size=VOCAB_SIZE,
+    num_layers=NUM_LAYERS,
+    dff=DFF,
+    d_model=D_MODEL,
+    num_heads=NUM_HEADS,
+    dropout=DROPOUT)
+
+model.summary()
+
+
+#모델 불러오기
+path = 'C:/Users/user/Desktop/web/python-deep-learning-chatbot/'
+model.load_weights(path + 'chatbot_transform_model_weights.h5')
+
+
+
+MAX_LENGTH = 40 
+def preprocess_sentence(sentence):
+  # 단어와 구두점 사이에 공백 추가.
+  # ex) 12시 땡! -> 12시 땡 !
+  sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+  sentence = sentence.strip()
+  return sentence
+
+def evaluate(sentence):
+  # 입력 문장에 대한 전처리
+  sentence = preprocess_sentence(sentence)
+
+  # 입력 문장에 시작 토큰과 종료 토큰을 추가
+  sentence = tf.expand_dims(
+      START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+
+  output = tf.expand_dims(START_TOKEN, 0)
+
+  # 디코더의 예측 시작
+  for i in range(MAX_LENGTH):
+    predictions = model(inputs=[sentence, output], training=False)
+
+    # 현재 시점의 예측 단어를 받아온다.
+    predictions = predictions[:, -1:, :]
+    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+    # 만약 현재 시점의 예측 단어가 종료 토큰이라면 예측을 중단
+    if tf.equal(predicted_id, END_TOKEN[0]):
+      break
+
+    # 현재 시점의 예측 단어를 output(출력)에 연결한다.
+    # output은 for문의 다음 루프에서 디코더의 입력이 된다.
+    output = tf.concat([output, predicted_id], axis=-1)
+
+  # 단어 예측이 모두 끝났다면 output을 리턴.
+  return tf.squeeze(output, axis=0)
+
+def predict(sentence):
+  prediction = evaluate(sentence)
+
+  # prediction == 디코더가 리턴한 챗봇의 대답에 해당하는 정수 시퀀스
+  # tokenizer.decode()를 통해 정수 시퀀스를 문자열로 디코딩.
+  predicted_sentence = tokenizer.decode(
+      [i for i in prediction if i < tokenizer.vocab_size])
+
+  # print('Input: {}'.format(sentence))
+  # print('Output: {}'.format(predicted_sentence))
+
+  return predicted_sentence
+
+
+
+from flask import Flask, render_template, jsonify, request
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
+
+
+@app.route('/', methods=["GET", "POST"])         #사이트 번호
+def index():
+    return render_template('index.html', **locals())
+
+
+
+@app.route('/chatbot', methods=["GET", "POST"])
+def chatbotResponse():
+
+    if request.method == 'POST':
+        the_question = request.form['question']
+
+        response = predict(the_question)
+
+    return jsonify({"response": response })
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8888', debug=True)
